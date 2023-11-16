95b2abc0ebafa4ff89fec10cfe00a0f3873f056c
Jang-Eun-Ji <jej132456@gmail.com>
2023-11-16 16:26:37 +0900

    DDL

---
+++ basic/4.DDL.sql
@@ -0,0 +1,16 @@
+INSERT into author(id, name, email) VALUES(1, 'kim', 'abc@naver.com');
+INSERT into author(id, name, email) VALUES(2, 'kim', 'abc@naver.com');
+INSERT into author(id, name, email) VALUES(3, 'kim', 'abc@naver.com');
+INSERT into author(id, name, email) VALUES(4, 'kim', 'abc@naver.com');
+INSERT into author(id, name, email) VALUES(5, 'kim', 'abc@naver.com');
+select *from author
+
+
+INSERT into post(id, title) VALUES(3, 'kim');
+INSERT into post(id, title) VALUES(4, 'kim');
+INSERT into post(id, author_id) VALUES(6,1);
+INSERT into post(id, author_id) VALUES(7, 2);
+
+select *from post
+INSERT into post(id, name, email) VALUES(5, 'kim', 'abc@naver.com');
+
