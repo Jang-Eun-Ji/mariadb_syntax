ad1a8c2bc6e7c877ae469e32a82c65745b6b4a5f
Jang-Eun-Ji <jej132456@gmail.com>
2023-11-16 15:54:28 +0900

    ..

---
+++ basic/2.DDL_practice.sql
@@ -0,0 +1,7 @@
+ALTER TABLE post modify COLUMN contents VARCHAR(3000);
+
+ALTER TABLE author CHANGE add COLUMN address VARCHAR(255);
+
+show creat TABLE author;
+
+DROP TABLE post;
---
+++ basic/3.DML.sql
@@ -0,0 +1,4 @@
+-- insert, select, update, delete
+INSTERT INTO 테이블명 (필드이름(칼럼명)1, 필드이름2,...) VALUES(데이터값1, 데이터값2....)
+INSERT into author(id, name, email) VALUES(1, 'kim', 'abc@naver.com');
+select *from author
